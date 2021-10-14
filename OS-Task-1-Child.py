c68b8089449aa2e513680ae63d76405667b843dc
Rifdar <assasinnyx@mail.ru>
2021-10-14 21:06:36 +0300

    added home1

---
+++ OS-Task-1-Child.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+import os
+import time
+import sys
+
+
+# In[2]:
+
+
+args = sys.argv;
+if (len(args) < 2):
+    print("Incorrect argument's amount");
+else:
+    print("Запущена программа Child в процессе с PID", os.getpid(),"с аргументом",waiting_period)
+    time.sleep(waiting_period);
+    exit_status = int(random.uniform(0, 2));
+    print("Завершился процесс с PID",os.getpid());
+    os._exit(exit_status);
+
+
+# In[ ]:
+
+
+
+
---
+++ OS-Task-1-Parent .py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import os
+import random
+import sys
+import time
+
+
+# In[8]:
+
+
+args = sys.argv;
+if (len(args) < 2):
+    print("Количество аргументов некорректно");
+else:
+    n = int(args[1]);
+    for i in range (0, n):
+        pr = os.fork();
+        if (pr > 0):
+            if i==0:
+                print("Запущена программа Parent с  PID",os.getpid());
+        else:
+            random_num = int(random.uniform(5, 11));
+            os.execl("./OS-Task-1-Child.py", "OS-Task-1-Child.py", str(random_num));
+            break;
+
+
+# In[9]:
+
+
+for i in range (0, n):
+    callback = os.wait();
+    print("Дочерний процесс с PID",callback[0],"завершился. Статус завершения",callback[0])
+
+
+# In[ ]:
+
+
+
+
